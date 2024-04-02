73f4025f5f75e563e5a76305d844a51fdc2d7735
nestortion <gerona.nestor@gmail.com>
2024-04-02 14:25:19 +0800

    [Accounting System]-[Add 5l link to clickable footer]

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,26 +1,27 @@
-import { FaRegCopyright } from "react-icons/fa6";
-import { text } from "./ui/text";
+import { FaRegCopyright } from 'react-icons/fa6'
+import { text } from './ui/text'
 
 function Footer() {
   return (
     <footer className="flex items-center justify-center h-[5vh]">
-      <div
-        className={text({
-          variant: "footer",
-          class: "flex",
-          style: "underline",
-        })}
-      >
-        <div className=" flex gap-1 pr-1">
-          Copyright <FaRegCopyright />
+      <a href="https://5lsolutions.com/">
+        <div
+          className={text({
+            variant: 'footer',
+            class: 'flex',
+            style: 'underline',
+          })}
+        >
+          <div className=" flex gap-1 pr-1">
+            Copyright <FaRegCopyright />
+          </div>
+          <div className="text-rose-800">
+            5L Solutions Supply's & Allied Services Inc.
+          </div>
         </div>
-        <div className="text-rose-800">
-          {" "}
-          5L Solutions Supply's & Allied Services Inc.
-        </div>
-      </div>
+      </a>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
