f39e88b39593813b984a1fbe7fb067a0f3acb4a0
Nestortion <gerona.nestor@gmail.com>
2024-04-22 12:12:10 +0800

    [Accounting System]-[Add error message when input value <= 0]

--- client/src/validators/employees.validator.ts
+++ client/src/validators/employees.validator.ts
@@ -1,32 +1,35 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const createEmployeeSchema = z.object({
-  empName: z.string().min(1, { message: 'required' }),
-  empAddress: z.string().min(1, { message: 'required' }),
-  empContactInfo: z.string().min(1, { message: 'required' }),
-  empEmail: z.string().email().min(1, { message: 'required' }),
+  empName: z.string().min(1, { message: "required" }),
+  empAddress: z.string().min(1, { message: "required" }),
+  empContactInfo: z.string().min(1, { message: "required" }),
+  empEmail: z.string().email().min(1, { message: "required" }),
   empBirthdate: z.date(),
   empDateHired: z.date(),
   empSalary: z
-    .union([z.number(), z.nan()])
+    .union([
+      z.number().positive({ message: "must not be 0 or negative" }),
+      z.nan(),
+    ])
     .refine((val) => !Number.isNaN(val), {
-      message: 'required',
+      message: "required",
     }),
-})
+});
 
 export const updateEmployeeSchema = z.object({
   empId: z.string().superRefine((val, ctx) => {
-    if (val.split(' ')[0] !== 'empId') {
+    if (val.split(" ")[0] !== "empId") {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: `Not an employee id.`,
-      })
+      });
     }
-    if (!z.string().uuid().safeParse(val.split(' ')[1]).success) {
+    if (!z.string().uuid().safeParse(val.split(" ")[1]).success) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: `Not valid uuid.`,
-      })
+      });
     }
   }),
   newData: z.object({
@@ -38,27 +41,29 @@ export const updateEmployeeSchema = z.object({
     empDateHired: z.date().optional(),
     empDateTerminated: z.date().optional(),
     empSalary: z
-      .union([z.number(), z.nan()])
+      .union([
+        z.number().positive({ message: "must not be 0 or negative" }),
+        z.nan(),
+      ])
       .refine((val) => !Number.isNaN(val), {
-        message: 'required',
-      })
-      .optional(),
+        message: "required",
+      }),
   }),
-})
+});
 
 export const terminateEmployeeSchema = z.object({
   empId: z.string().superRefine((val, ctx) => {
-    if (val.split(' ')[0] !== 'empId') {
+    if (val.split(" ")[0] !== "empId") {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: `Not an employee id.`,
-      })
+      });
     }
-    if (!z.string().uuid().safeParse(val.split(' ')[1]).success) {
+    if (!z.string().uuid().safeParse(val.split(" ")[1]).success) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: `Not valid uuid.`,
-      })
+      });
     }
   }),
-})
+});
