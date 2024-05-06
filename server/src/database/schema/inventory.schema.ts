6b91f5c9cf585fb30186a64c487d18dcbb69459e
nestortion <gerona.nestor@gmail.com>
2024-05-06 11:02:20 +0800

    [Accounting System]-[Add price per unit in inventory schema]

--- server/src/database/schema/inventory.schema.ts
+++ server/src/database/schema/inventory.schema.ts
@@ -1,10 +1,17 @@
-import { int, mysqlEnum, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import {
+  decimal,
+  int,
+  mysqlEnum,
+  mysqlTable,
+  varchar,
+} from "drizzle-orm/mysql-core";
 
 const inventory = mysqlTable("inventory", {
   invId: varchar("inv_id", { length: 60 }).primaryKey(),
   invAssetName: varchar("inv_asset_name", { length: 40 }).notNull().unique(),
   invStocks: int("inv_stocks").notNull(),
   invStatus: mysqlEnum("inv_status", ["GOOD", "WARNING", "DEPLETED"]),
+  invPricePerUnit: decimal("inv_price_per_unit", { precision: 13, scale: 2 }),
 });
 
 export default inventory;
