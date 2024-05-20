bc37047b65c5c52d92f5fd1dbf27bd599b44dea5
nestortion <gerona.nestor@gmail.com>
2024-05-20 11:20:33 +0800

    [Accounting System]-[Change sync employees by API dynamic]

--- server/src/controller/others.controllers.ts
+++ server/src/controller/others.controllers.ts
@@ -302,56 +302,63 @@ export const downloadFile = async (req: Request, res: Response) => {
 };
 
 export const syncEmployeesByAPI = async (req: Request, res: Response) => {
-  const data = await fetch(`http://172.16.2.200:3005/employee/load`).then(
-    (res) =>
-      res.json() as Promise<{
-        msg: string;
-        data: Array<{
-          newEmployeeId: string;
-          firstname: string;
-          phone: string;
-          email: string;
-          jobstatus: string;
-          me_department: string;
-          me_position: string;
-        }>;
-      }>
-  );
+  const api = req.query.employeeApi as string;
+  try {
+    const data = await fetch(api).then(
+      (res) =>
+        res.json() as Promise<{
+          msg: string;
+          data: Array<{
+            newEmployeeId: string;
+            firstname: string;
+            phone: string;
+            email: string;
+            jobstatus: string;
+            me_department: string;
+            me_position: string;
+          }>;
+        }>
+    );
 
-  const shapeDataToDB = data.data.map((emp) => ({
-    empId: emp.newEmployeeId,
-    empName: emp.firstname,
-    empContactInfo: emp.phone,
-    empEmail: emp.email,
-    empJobStatus: emp.jobstatus,
-    empDepartment: emp.me_department,
-    empPosition: emp.me_position,
-  }));
-  // console.log(shapeDataToDB);
+    const shapeDataToDB = data.data.map((emp) => ({
+      empId: emp.newEmployeeId,
+      empName: emp.firstname,
+      empContactInfo: emp.phone,
+      empEmail: emp.email,
+      empJobStatus: emp.jobstatus,
+      empDepartment: emp.me_department,
+      empPosition: emp.me_position,
+    }));
 
-  await Promise.all(
-    shapeDataToDB.map(async (emp) => {
-      //check if employee exists
-      const empExists = await db.query.employees.findFirst({
-        where: eq(employees.empId, emp.empId),
-      });
+    await Promise.all(
+      shapeDataToDB.map(async (emp) => {
+        //check if employee exists
+        const empExists = await db.query.employees.findFirst({
+          where: eq(employees.empId, emp.empId),
+        });
 
-      //early return if employee does not exist
-      if (empExists) return;
+        //early return if employee does not exist
+        if (empExists) return;
 
-      //insert employee
-      const newEmployee = await db.insert(employees).values({
-        ...emp,
-        empDateHired: new Date(),
-      });
+        //insert employee
+        const newEmployee = await db.insert(employees).values({
+          ...emp,
+          empDateHired: new Date(),
+        });
 
-      return newEmployee;
-    })
-  );
+        return newEmployee;
+      })
+    );
 
-  //query all synced employees
-  const syncedEmployees = await db.query.employees.findMany();
+    //query all synced employees
+    const syncedEmployees = await db.query.employees.findMany();
 
-  //return synced employees
-  return res.status(200).send({ syncedEmployees });
+    //return synced employees
+    return res.status(200).send({ syncedEmployees });
+  } catch (error) {
+    console.log("error in syncing employees");
+    console.log(error);
+
+    return res.status(500).send({ error: "Server error" });
+  }
 };
