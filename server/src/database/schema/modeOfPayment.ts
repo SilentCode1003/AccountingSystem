51e9bfbee3e0b8b5848a5577c79a3ad2f933af26
nestortion <gerona.nestor@gmail.com>
2024-05-21 09:34:29 +0800

    [Accounting System]-[Add modeOfPayment is active]

--- server/src/database/schema/modeOfPayment.ts
+++ server/src/database/schema/modeOfPayment.ts
@@ -1,10 +1,11 @@
 import { relations } from "drizzle-orm";
-import { mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { boolean, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 import transactions from "./transactions.schema";
 
 const modesOfPayment = mysqlTable("modes_of_payment", {
   mopId: varchar("mop_id", { length: 60 }).primaryKey(),
   mopName: varchar("mop_name", { length: 60 }).notNull(),
+  mopIsActive: boolean("mop_is_active").notNull().default(true),
 });
 
 export const modesOfPaymentTransactionRelations = relations(
