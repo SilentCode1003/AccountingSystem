518475f69baeadcc56ff3da3c949c918951d8663
nestortion <gerona.nestor@gmail.com>
2024-05-27 16:32:50 +0800

    [Accounting System]-[Add email to user schema]

--- server/src/database/schema/users.schema.ts
+++ server/src/database/schema/users.schema.ts
@@ -11,6 +11,7 @@ const users = mysqlTable("users", {
   userType: mysqlEnum("user_type", ["FINANCE", "HIGHER_DEPARTMENT"]),
   userUsername: varchar("user_username", { length: 16 }).notNull().unique(),
   userFullName: varchar("user_fullname", { length: 50 }).notNull(),
+  userEmail: varchar("user_email", { length: 50 }).notNull().unique(),
   userProfilePic: text("user_profile_pic").notNull().default("default-img.png"),
   userContactNumber: varchar("user_contact_number", { length: 15 }).notNull(),
   userPassword: varchar("user_password", { length: 60 }).notNull(),
