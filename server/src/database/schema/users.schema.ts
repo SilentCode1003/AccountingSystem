007610ca6363c098088cce35808a3655772de502
nestortion <gerona.nestor@gmail.com>
2024-05-27 11:08:52 +0800

    [Accounting System]-[Change userUsername to unique]

--- server/src/database/schema/users.schema.ts
+++ server/src/database/schema/users.schema.ts
@@ -9,7 +9,7 @@ import {
 const users = mysqlTable("users", {
   userId: varchar("user_id", { length: 60 }).primaryKey(),
   userType: mysqlEnum("user_type", ["FINANCE", "HIGHER_DEPARTMENT"]),
-  userUsername: varchar("user_username", { length: 16 }).notNull(),
+  userUsername: varchar("user_username", { length: 16 }).notNull().unique(),
   userFullName: varchar("user_fullname", { length: 50 }).notNull(),
   userProfilePic: text("user_profile_pic").notNull().default("default-img.png"),
   userContactNumber: varchar("user_contact_number", { length: 15 }).notNull(),
