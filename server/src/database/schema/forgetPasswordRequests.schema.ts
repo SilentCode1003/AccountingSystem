9b8757409feb5f833596afbf931ddb11cea8eb49
nestortion <gerona.nestor@gmail.com>
2024-05-28 14:18:21 +0800

    [Accounting System]-[Change forgetpasswordrequests: reference userId to users schema]

--- server/src/database/schema/forgetPasswordRequests.schema.ts
+++ server/src/database/schema/forgetPasswordRequests.schema.ts
@@ -1,8 +1,18 @@
-import { datetime, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import {
+  AnyMySqlColumn,
+  datetime,
+  mysqlTable,
+  varchar,
+} from "drizzle-orm/mysql-core";
+import users from "./users.schema";
 
 const forgetPasswordRequests = mysqlTable("forget_password_requests", {
   id: varchar("id", { length: 60 }).primaryKey(),
-  userId: varchar("user_id", { length: 60 }).notNull(),
+  userId: varchar("user_id", { length: 60 })
+    .references((): AnyMySqlColumn => users.userId, {
+      onDelete: "cascade",
+    })
+    .notNull(),
   timeRequested: datetime("time_requested").notNull(),
 });
 
