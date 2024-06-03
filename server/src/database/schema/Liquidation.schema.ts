29033f5f276524bd4446eed5e298b09c02ab18a6
nestortion <gerona.nestor@gmail.com>
2024-06-03 14:57:21 +0800

    [Accounting System]-[Remove liquidationRT from liquidation schema]

--- server/src/database/schema/Liquidation.schema.ts
+++ server/src/database/schema/Liquidation.schema.ts
@@ -12,7 +12,6 @@ import liquidationRoutes from "./liquidationRoutes.schema";
 
 const liquidations = mysqlTable("liquidation", {
   liquidationId: varchar("liquidation_id", { length: 60 }).primaryKey(),
-  liquidationRT: varchar("liquidation_rt", { length: 60 }).notNull(),
   liquidationTranId: varchar("liquidation_tran_id", { length: 60 })
     .references((): AnyMySqlColumn => transactions.tranId, {
       onDelete: "cascade",
