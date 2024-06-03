5f63b179fd40cccc85976a8daff8f912cb90d839
nestortion <gerona.nestor@gmail.com>
2024-06-03 15:30:00 +0800

    [Accounting System]-[Change routes start and end unique pairs]

--- server/src/database/schema/routes.schema.ts
+++ server/src/database/schema/routes.schema.ts
@@ -1,13 +1,19 @@
-import { decimal, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { decimal, mysqlTable, unique, varchar } from "drizzle-orm/mysql-core";
 
-const routes = mysqlTable("routes", {
-  routeId: varchar("route_id", { length: 60 }).primaryKey(),
-  routeStart: varchar("route_start", { length: 60 }),
-  routeEnd: varchar("route_end", { length: 60 }),
-  routePrice: decimal("route_price", { precision: 13, scale: 2 })
-    .notNull()
-    .$type<number>(),
-  routeModeOfTransport: varchar("route_mode_of_transport", { length: 60 }),
-});
+const routes = mysqlTable(
+  "routes",
+  {
+    routeId: varchar("route_id", { length: 60 }).primaryKey(),
+    routeStart: varchar("route_start", { length: 60 }),
+    routeEnd: varchar("route_end", { length: 60 }),
+    routePrice: decimal("route_price", { precision: 13, scale: 2 })
+      .notNull()
+      .$type<number>(),
+    routeModeOfTransport: varchar("route_mode_of_transport", { length: 60 }),
+  },
+  (t) => ({
+    unq: unique().on(t.routeStart, t.routeEnd),
+  })
+);
 
 export default routes;
