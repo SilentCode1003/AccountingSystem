a2b1ce76288ae19ef1eb0ec1215d0f26468f21fa
nestortion <gerona.nestor@gmail.com>
2024-06-05 10:35:00 +0800

    [Accounting System]-[Add employeeLiquidation route and controller]

--- server/src/controller/liquidations.controller.ts
+++ server/src/controller/liquidations.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { getAllLiquidations } from "../database/services/liquidations.service";
 import db from "../database";
+import { and, desc, eq, getTableColumns, sql, sum } from "drizzle-orm";
+import employees from "../database/schema/employees.schema";
+import liquidations from "../database/schema/Liquidation.schema";
 
 export const getLiquidations = async (req: Request, res: Response) => {
   try {
@@ -9,6 +12,67 @@ export const getLiquidations = async (req: Request, res: Response) => {
     return res.status(200).send({ liquidations });
   } catch (error) {
     console.log("error in fetching all liquidations");
+    console.log(error);
+    return res.status(500).send({ error: "Server error" });
+  }
+};
+
+export const getEmployeeLiquidations = async (req: Request, res: Response) => {
+  const date = new Date(req.query.date as string);
+  try {
+    let employeeLiquidations: Array<{
+      employee: {
+        empId: string;
+        empName: string | null;
+      };
+      amount: string | null;
+    }> = [];
+
+    if (!req.query.date) {
+      employeeLiquidations = await db
+        .select({
+          employee: {
+            empName: getTableColumns(employees).empName,
+            empId: getTableColumns(employees).empId,
+          },
+          amount: sum(liquidations.liquidationAmount),
+        })
+        .from(liquidations)
+        .groupBy(liquidations.liquidationEmpId)
+        .orderBy(({ amount }) => desc(amount))
+        .innerJoin(
+          employees,
+          eq(employees.empId, liquidations.liquidationEmpId)
+        );
+    } else {
+      employeeLiquidations = await db
+        .select({
+          employee: {
+            empName: getTableColumns(employees).empName,
+            empId: getTableColumns(employees).empId,
+          },
+          amount: sum(liquidations.liquidationAmount),
+        })
+        .from(liquidations)
+        .where(
+          and(
+            eq(sql`month(liquidation_date)`, date.getMonth() + 1),
+            eq(sql`year(liquidation_date)`, date.getFullYear())
+          )
+        )
+        .groupBy(liquidations.liquidationEmpId)
+        .innerJoin(
+          employees,
+          eq(employees.empId, liquidations.liquidationEmpId)
+        );
+    }
+
+    console.log("successfully fetched all employee liquidations");
+    return res.status(200).send({
+      employeeLiquidations,
+    });
+  } catch (error) {
+    console.log("error in fetching all employee liquidations");
     console.log(error);
     return res.status(500).send({ error: "Server error" });
   }
--- server/src/routes/liquidations.routes.ts
+++ server/src/routes/liquidations.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getLiquidations } from "../controller/liquidations.controller";
+import {
+  getEmployeeLiquidations,
+  getLiquidations,
+} from "../controller/liquidations.controller";
 
 const liquidationRouter = Router();
 
 //get all liquidations
 liquidationRouter.get("/", getLiquidations);
+
+//get all liquidations for an employee
+liquidationRouter.get("/employeeLiquidations", getEmployeeLiquidations);
 
 export default liquidationRouter;
